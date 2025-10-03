e22fd1ef466ef24d294c145fcba2cdd256dd7709
ARN <hmv619@alumni.ku.dk>
2025-10-03 11:49:51 +0200

    fixed perplexity

--- nlm/probs.py
+++ nlm/probs.py
@@ -1,5 +1,6 @@
 import math
 import torch
+from decimal import Decimal, localcontext
 
 def sentence_log_probability(model, device, tokenizer, sentence: str) -> tuple[float, int]:
     """Calculate the log probability of a sentence using the language model."""
@@ -19,15 +20,14 @@ def sentence_log_probability(model, device, tokenizer, sentence: str) -> tuple[f
     return log_prob, int(N)
 
 def perplexity(model, device, tokenizer, corpus: list[str]) -> float:
-    '''
-    Compute perplexity of a sentence under the language model.
-    sentence_ids: torch.LongTensor of shape (1, seq_len)
-    sentence_len: torch.LongTensor of shape (1,)
-    '''
     prob_sum = 0.0
     N = 0
     for sentence in corpus:
         log_prob, running_n = sentence_log_probability(model, device, tokenizer, sentence) 
-        prob_sum += math.exp(log_prob)
+        prob_sum += log_prob
         N += running_n
-    return math.pow(prob_sum, -(1/N))
+
+    with localcontext() as ctx:
+        ctx.prec = 50
+        corpus_prob = Decimal(prob_sum).exp()
+        return corpus_prob ** (Decimal(-1) / Decimal(N))
